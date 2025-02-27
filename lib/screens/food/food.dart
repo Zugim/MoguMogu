ab9d6bde88793f030a15fa446a605ea8526a67d7
Joe Reeve <joe.reeve@gmail.com>
2025-02-27 16:13:16 +0900

    feat: filter food by type

--- lib/screens/food/food.dart
+++ lib/screens/food/food.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mogu_mogu/models/food.dart';
 import 'package:mogu_mogu/screens/food/food_card.dart';
 import 'package:mogu_mogu/screens/food_profile/food_profile.dart';
 import 'package:mogu_mogu/shared/styled_text.dart';
+import 'package:mogu_mogu/theme.dart';
 
 class Food extends StatefulWidget {
   const Food({super.key});
@@ -12,6 +14,9 @@ class Food extends StatefulWidget {
 }
 
 class _FoodState extends State<Food> {
+  String dropdownValue = "all";
+  var filteredFoods = List.from(foodItems);
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -22,23 +27,68 @@ class _FoodState extends State<Food> {
             margin: EdgeInsets.only(bottom: 10),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [StyledTitle("Baby Safe Food")],
+              children: [
+                StyledTitle("Baby Safe Food"),
+                DropdownButton(
+                  value: dropdownValue,
+                  style: GoogleFonts.vollkorn(
+                    textStyle: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  underline: Container(height: 2, color: AppColors.textColor),
+                  icon: Icon(
+                    Icons.filter_alt_sharp,
+                    size: 15,
+                    color: AppColors.textColor,
+                  ),
+                  items: [
+                    DropdownMenuItem<String>(value: "all", child: Text("All")),
+                    DropdownMenuItem<String>(
+                      value: "fruit",
+                      child: Text("Fruit"),
+                    ),
+                    DropdownMenuItem<String>(value: "veg", child: Text("Veg")),
+                    DropdownMenuItem<String>(
+                      value: "meat",
+                      child: Text("Meat"),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: "fish",
+                      child: Text("Fish"),
+                    ),
+                  ],
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      dropdownValue = newValue!;
+
+                      if (newValue == "all") {
+                        filteredFoods = List.from(foodItems);
+                      } else {
+                        filteredFoods = List.from(
+                          foodItems.where((food) => food.type == newValue),
+                        );
+                      }
+
+                      print(filteredFoods[0].name);
+                    });
+                  },
+                ),
+              ],
             ),
           ),
           Expanded(
             child: ListView.builder(
-              itemCount: foodItems.length,
+              itemCount: filteredFoods.length,
               itemBuilder: (_, index) {
                 return GestureDetector(
                   onTap: () {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => FoodProfile(foodItems[index]),
+                        builder: (context) => FoodProfile(filteredFoods[index]),
                       ),
                     );
                   },
-                  child: FoodCard(foodItems[index]),
+                  child: FoodCard(filteredFoods[index]),
                 );
               },
             ),
